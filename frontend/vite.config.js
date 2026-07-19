5858556a399a5fdf449682294cbcf290001c65e4
Jay Chand Maurya <jaychandmaurya0105@gmail.com>
2026-07-19 11:57:37 +0530

    allow render host in vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      "ai-email-generator-2-qgg6.onrender.com"
+    ]
+  }
 })
